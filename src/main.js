707706ac6ebd1158a28123bb2f05ac4e82028660
Sanaa-Uzuri Robotham <sanaauzurirm@gmail.com>
2026-02-12 13:35:54 -0500

    Finished main/js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 // Import functions
 import { getProducts } from './fetch-helpers.js'
 import { renderProducts } from './dom-helpers.js'
+
+getProducts()
+    .then(result => {
+        if (result.error) {
+            const errorElement = document.querySelector('#error-message')
+            errorElement.textContent = result.error.message
+        } else {
+             renderProducts(result.data)
+        }
+    })
